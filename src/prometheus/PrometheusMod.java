7564b1a2db6884ef8106fc5ae91cdc0fbccee4a4
PixaxeOfPixie <63517945+pixaxeofpixie@users.noreply.github.com>
2021-06-02 21:44:16 +0300

    Add files via upload

---
+++ src/prometheus/PrometheusMod.java
@@ -0,0 +1,14 @@
+package prometheus;
+
+import mindustry.mod.Mod;
+
+public class PrometheusMod extends Mod{
+
+    @Override
+    public void init(){
+    }
+
+    @Override
+    public void loadContent(){
+    }
+}
