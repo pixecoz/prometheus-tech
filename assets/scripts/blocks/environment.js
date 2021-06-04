c4260c356d2b8a675839260c035f09dd1594f9b4
SuperJaba2000 <73773387+SuperJaba2000@users.noreply.github.com>
2021-06-04 18:00:05 +0300

    Update environment.js

--- assets/scripts/blocks/environment.js
+++ assets/scripts/blocks/environment.js
@@ -2,12 +2,12 @@
 
 //FUNCTIONS//
 
-function newFloor(name, variants, speedMultiplier){
+function newFloor(name, variants, speedMultiplier, emitLight, lightRadius){
   const floor = extendContent(Floor, name, {});
   floor.variants = variants;
   floor.speedMultiplier = speedMultiplier;
-  
-  
+  floor.emitLight = emitLight;
+  floor.lightRadius = lightRadius;
   
   return floor;
 };
@@ -19,7 +19,20 @@ function newLiquidFloor(name, variants, speedMultiplier,){
   liqFloor.speedMultiplier = speedMultiplier;
   //SuperJaba2000 НАПИШЕТ!
   
-  return liqFloor
+  return liqFloor;
 };
+
+function newBlock(name, variants, solid){
+  const StaticBlock = extendContent(StaticWall, name, {});
+  StaticBlock.variants = variants;
+  StaticBlock.solid = solid;
+  
+  return StaticBlock;
+};
+
 //ATTEIBUTES and OTHER write separately!!!
 
+//CONTENT//
+
+const whiteStoneWall = newBlock("white-stone-block", 1, true);
+const whiteStoneFloor = newFloor("white-stone-floor", 2, 1.0, false, 0.0);
