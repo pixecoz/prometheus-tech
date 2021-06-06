5da35c20d5cd6f6170ff805c97d68dade5bf00ef
PixaxeOfPixie <63517945+pixaxeofpixie@users.noreply.github.com>
2021-06-06 21:10:30 +0300

    Update Koluro.js

--- assets/scripts/Koluro.js
+++ assets/scripts/Koluro.js
@@ -2,8 +2,8 @@ const Koluro = new JavaAdapter(Planet, {}, "Koluro", Planets.sun, 3, 1.6);
 
 Koluro.hasAtmosphere = false;
 Koluro.meshLoader = () => new SunMesh( Koluro, 4, 5, 0.3, 1.7, 1.2, 1, 1.5, 
-                                      Color.valueOf("f7c2f9"), 
-                                      Color.valueOf("f294f7"), 
+                                      Color.valueOf("e693ea"), 
+                                      Color.valueOf("fbbcff"), 
                                       Color.valueOf("e4aded"), 
                                       Color.valueOf("f9e8f9"),
                                       Color.valueOf("cf9cd3"),
