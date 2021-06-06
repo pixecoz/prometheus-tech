b0f010f2b59f0a49bdd6e9c4bfa58333104ef00e
PixaxeOfPixie <63517945+pixaxeofpixie@users.noreply.github.com>
2021-06-06 20:29:50 +0300

    new hex-es

--- assets/scripts/Koluro.js
+++ assets/scripts/Koluro.js
@@ -1,7 +1,13 @@
 const Koluro = new JavaAdapter(Planet, {}, " Koluro", Planets.sun, 3, 1.6);
 
 Koluro.hasAtmosphere = false;
-Koluro.meshLoader = () => new SunMesh( Koluro, 4, 5, 0.3, 1.7, 1.2, 1, 1.5, Color.valueOf("EB00B8"), Color.valueOf("FF80E3"), Color.valueOf("CC80FF"), Color.valueOf("F6A3FF"));
+Koluro.meshLoader = () => new SunMesh( Koluro, 4, 5, 0.3, 1.7, 1.2, 1, 1.5, 
+                                      Color.valueOf("fbe0ff"), 
+                                      Color.valueOf("f1caf7"), 
+                                      Color.valueOf("e4aded"), 
+                                      Color.valueOf("de98ea"),
+                                      Color.valueOf("ce77dd"),
+                                      Color.valueOf("d3a4db"));
 Koluro.orbitRadius = 27.3;
 Koluro.accessible = false;
 Koluro.bloom = true;
