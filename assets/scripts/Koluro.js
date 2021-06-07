e39310fb2f7761651432b880dab7e298cf0dadbd
SuperJaba2000 <73773387+SuperJaba2000@users.noreply.github.com>
2021-06-07 07:30:34 +0300

    Update Koluro.js

--- assets/scripts/Koluro.js
+++ assets/scripts/Koluro.js
@@ -1,13 +1,15 @@
-const Koluro = new Planet("Koluro", Planets.sun, 3, 1.6);
+const Koluro = new Planet("Koluro", Planets.sun, 3, 1.6{{
 
-Koluro.hasAtmosphere = false;
-Koluro.meshLoader = () => new SunMesh( Koluro, 4, 5, 0.3, 1.7, 1.2, 1, 1.5, 
-                                      Color.valueOf("e693ea"), 
-                                      Color.valueOf("fbbcff"), 
-                                      Color.valueOf("e4aded"), 
-                                      Color.valueOf("f9e8f9"),
-                                      Color.valueOf("cf9cd3"),
-                                      Color.valueOf("cd92ce"));
-Koluro.orbitRadius = 27.3;
-Koluro.accessible = false;
-Koluro.bloom = true;
+meshLoader = () => new SunMesh( 
+    Koluro, 4, 5, 0.3, 1.7, 1.2, 1, 1.1, 
+    Color.valueOf("e693ea"), 
+    Color.valueOf("fbbcff"), 
+    Color.valueOf("e4aded"), 
+    Color.valueOf("f9e8f9"),
+    Color.valueOf("cf9cd3"),
+    Color.valueOf("cd92ce"));
+);
+orbitRadius = 27.3;
+accessible = false;
+bloom = true;
+}};
