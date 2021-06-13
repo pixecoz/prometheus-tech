bda127d006f0a56f7a38dc4c90162d5159713fc9
1qwaka <yalenin01@gmail.com>
2021-06-13 15:07:40 +0500

    цывмаиуаиу

--- src/prometheus/content/PrtFx.java
+++ src/prometheus/content/PrtFx.java
@@ -26,7 +26,7 @@ public class PrtFx {
             float m = i == 0 ? 1.0F : 0.5F;
             float rot = e.rotation + 180.0F;
             float w = 20.0F * e.fout() * m;
-            Drawf.tri(e.x, e.y, w, (35.0F + Mathf.randomSeedRange((long) e.id, 19.0F)) * m, rot);
+            Drawf.tri(e.x, e.y, w, (35.0F + Mathf.randomSeedRange(e.id, 19.0F)) * m, rot);
             Drawf.tri(e.x, e.y, w, 15.0F * m, 90);
         }
 
@@ -35,7 +35,7 @@ public class PrtFx {
     dystopiaShoot = new Effect(60.0F, (e) -> {
         e.scaled(32.0F, (b) -> {
             Draw.color(Color.white, PrtColors.dystopiaFrontColor, b.fin());
-            Lines.stroke(b.fout() * 5.0F + 1.7F);
+            Lines.stroke(b.fout() * 6.5F);
             Lines.circle(b.x, b.y, b.fin() * 50.0F);
             Lines.square(e.x, e.y, b.fin() * 40.0F, 60);
         });
@@ -61,9 +61,9 @@ public class PrtFx {
             float m = i == 0 ? 1.0F : 0.5F;
 
             for (int j = 0; j < 5; ++j) {
-                float rot = e.rotation + Mathf.randomSeedRange((long) (e.id + j), 50.0F);
+                float rot = e.rotation + Mathf.randomSeedRange(e.id + j, 50.0F);
                 float w = 23.0F * e.fout() * m;
-                Drawf.tri(e.x, e.y, w, (80.0F + Mathf.randomSeedRange((long) (e.id + j), 40.0F)) * m, rot);
+                Drawf.tri(e.x, e.y, w, (80.0F + Mathf.randomSeedRange(e.id + j, 40.0F)) * m, rot);
                 Drawf.tri(e.x, e.y, w, 20.0F * m, rot + 180.0F);
                 Drawf.circles(e.x, e.y, 40);
             }
@@ -76,7 +76,7 @@ public class PrtFx {
         });
         e.scaled(12.0F, (c) -> {
             Draw.color(PrtColors.dystopiaColor);
-            randLenVectors((long) e.id, 25, 5.0F + e.fin() * 80.0F, e.rotation, 60.0F, (x, y) -> {
+            randLenVectors(e.id, 25, 5.0F + e.fin() * 80.0F, e.rotation, 60.0F, (x, y) -> {
                 Fill.square(e.x + x, e.y + y, c.fout() * 3.0F, 45.0F);
             });
         });
