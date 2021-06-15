02de69e15617cfb5d302b65567b8b64c79983004
stormtrooper145 <57406945+stormtrooper145@users.noreply.github.com>
2021-06-15 20:24:32 +0300

    Fixes

--- src/prometheus/entities/TurretEffects.java
+++ src/prometheus/entities/TurretEffects.java
@@ -8,25 +8,26 @@ import arc.math.geom.Vec2;
 import arc.util.Log;
 import arc.util.Tmp;
 import mindustry.entities.Effect;
+import mindustry.entities.Units;
+import mindustry.gen.Posc;
 import mindustry.graphics.Pal;
 
 public class TurretEffects {
 
-    public static void beamsCharge(float x, float y, float tx, float ty, float rotation){
-        Effect effect = new Effect(190f,100f, e-> {
+    public static void beamsCharge(float x, float y, Posc t, float rotation){
+        Effect effect = new Effect(100f,100f, e-> {
             Tmp.v1.set(0, 10);
             Tmp.v2.set(0, -10);
             Tmp.v3.set(10, 0);
 
-            float len1 = Tmp.v1.set(Angles.trnsx(rotation + 90, Tmp.v1.x, Tmp.v1.y) + e.x, Angles.trnsy(rotation + 90, Tmp.v1.x, Tmp.v1.y) + e.y).dst(tx, ty);
-            float len2 = Tmp.v2.set(Angles.trnsx(rotation + 90, Tmp.v2.x, Tmp.v2.y) + e.x, Angles.trnsy(rotation + 90, Tmp.v2.x, Tmp.v2.y) + e.y).dst(tx, ty);
-            float len3 = Tmp.v3.set(Angles.trnsx(rotation + 90, Tmp.v3.x, Tmp.v3.y) + e.x, Angles.trnsy(rotation + 90, Tmp.v3.x, Tmp.v3.y) + e.y).dst(tx, ty);
+            float len1 = Tmp.v1.set(Angles.trnsx(rotation + 90, Tmp.v1.x, Tmp.v1.y) + e.x, Angles.trnsy(rotation + 90, Tmp.v1.x, Tmp.v1.y) + e.y).dst(t.x(), t.y());
+            float len2 = Tmp.v2.set(Angles.trnsx(rotation + 90, Tmp.v2.x, Tmp.v2.y) + e.x, Angles.trnsy(rotation + 90, Tmp.v2.x, Tmp.v2.y) + e.y).dst(t.x(), t.y());
+            float len3 = Tmp.v3.set(Angles.trnsx(rotation + 90, Tmp.v3.x, Tmp.v3.y) + e.x, Angles.trnsy(rotation + 90, Tmp.v3.x, Tmp.v3.y) + e.y).dst(t.x(), t.y());
 
-
-            float ang1 = (Tmp.v3.angleTo(tx, ty)) - 15 * e.fout();
-            float ang2 = (Tmp.v3.angleTo(tx, ty)) + 15 * e.fout();
-            float ang3 = (Tmp.v1.angleTo(tx, ty)) + 30 * e.fout();
-            float ang4 = (Tmp.v2.angleTo(tx, ty)) - 30 * e.fout();
+            float ang1 = (Tmp.v3.angleTo(t.x(), t.y())) - 15 * e.fout();
+            float ang2 = (Tmp.v3.angleTo(t.x(), t.y())) + 15 * e.fout();
+            float ang3 = (Tmp.v1.angleTo(t.x(), t.y())) + 30 * e.fout();
+            float ang4 = (Tmp.v2.angleTo(t.x(), t.y())) - 30 * e.fout();
 
             Tmp.v1.set(0, 10);
             Tmp.v2.set(0, -10);
--- src/prometheus/world/blocks/turrets/ChargeTurret.java
+++ src/prometheus/world/blocks/turrets/ChargeTurret.java
@@ -1,6 +1,13 @@
 package prometheus.world.blocks.turrets;
 
+import arc.graphics.g2d.Draw;
+import arc.graphics.g2d.Lines;
+import arc.math.Angles;
 import arc.util.Log;
+import arc.util.Tmp;
+import mindustry.entities.Effect;
+import mindustry.entities.Units;
+import mindustry.graphics.Pal;
 import mindustry.world.blocks.defense.turrets.ItemTurret;
 import prometheus.entities.TurretEffects;
 
@@ -12,11 +19,10 @@ public class ChargeTurret extends ItemTurret {
         public boolean effectDone = false;
         @Override
         public void draw(){
-            Log.info(chargeTime);
             super.draw();
-            if(target != null && !effectDone){
+            if(target != null && !effectDone && charging){
                 effectDone = true;
-                TurretEffects.beamsCharge(x, y, target.x(), target.y(), rotation - 90);
+                TurretEffects.beamsCharge(x, y, target, rotation - 90);
             }
             else if(target == null){
                 effectDone = false;
