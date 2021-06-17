da3144480faa538d31ea74c492257ce6d46473be
1qwaka <yalenin01@gmail.com>
2021-06-17 16:58:02 +0500

    commit

---
+++ src/prometheus/entities/units/Aboltus.java
@@ -0,0 +1,12 @@
+package prometheus.entities.units;
+
+import mindustry.gen.UnitEntity;
+import static prometheus.PrtFuncs.print;
+
+public class Aboltus extends UnitEntity {
+    @Override
+    public void update() {
+        super.update();
+        print("абобус");
+    }
+}
