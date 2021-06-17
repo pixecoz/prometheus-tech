8c0f873be73f2ac772e414d23a188c489c38a404
1qwaka <yalenin01@gmail.com>
2021-06-17 17:59:17 +0500

    аболтус не работает

---
+++ src/prometheus/entities/ExtendedUnitType.java
@@ -0,0 +1,11 @@
+package prometheus.entities;
+
+import mindustry.type.UnitType;
+
+public class ExtendedUnitType extends UnitType {
+    public int classId;
+    public ExtendedUnitType(String name){
+        super(name);
+
+    }
+}
