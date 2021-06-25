90b085035bdc1ba130137f6d3983e8707a5e8f93
KOCTOLOM <76535433+KOCTOLOM@users.noreply.github.com>
2021-06-25 14:20:55 +0300

    КТО В КАМПАНИЮ АДАМА ПИХНУЛ А?

--- src/prometheus/content/PrtBlocks.java
+++ src/prometheus/content/PrtBlocks.java
@@ -313,7 +313,7 @@ public class PrtBlocks implements ContentList {
             range = 460;
             requirements(Category.turret, ItemStack.with(Items.copper, 1500, Items.lead, 2000, Items.metaglass, 500, PrtItems.platinum, 400, Items.silicon, 800, Items.plastanium, 400));
             itemCapacity = 1000;
-            buildVisibility = BuildVisibility.campaignOnly;
+            buildVisibility = BuildVisibility.sandboxOnly;
             shootEffect = PrtFx.orbitalLaserCharge;
             ammo(
                     Items.surgeAlloy, new PodStat(){{
