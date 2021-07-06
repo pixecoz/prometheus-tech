ef8f6e2cd5e30a6baab7efc7e33a46113ae35b0c
SuperJaba2000 <73773387+SuperJaba2000@users.noreply.github.com>
2021-07-06 14:06:52 +0300

    Planets to tech tree

--- assets/scripts/techTree.js
+++ assets/scripts/techTree.js
@@ -1,5 +1,12 @@
 ///CONSTANTS///
 
+Events.on(ClientLoadEvent, cons(e=>{
+    const Koluro = Vars.content.getByName(ContentType.planet,"prometheus-koluro");
+    const CarputoPlanet = Vars.content.getByName(ContentType.planet,"prometheus-CarputoPlanet");
+    
+    const testSector = Vars.content.getByName(ContentType.sector,"prometheus-testSector");
+}));
+
 ///FUNCTIONS///
 
 function changeParent(child, newParent){
@@ -20,21 +27,41 @@ const PB = extendContent(Wall, name, {});
 PB.size = TexturePixelSize/32;
 //not change
 PB.health = 0.1;
-PB.buildVisibility = BuildVisibility.editorOnly;
+PB.buildVisibility = BuildVisibility.hidden;
 return PB;
 };
 
 ///TECH TREE///
 
+//MOD TECH TREE//
+
 const SunStarIcon = createPhantomBlock("SunStarIcon", 64);
-SunStarIcon.alwaysUnlocked = true;
+SunStarIcon.alwaysUnlocked = true;//SUN TO TECH TREE
 
 const SerpuloPlanetIcon = createPhantomBlock("SerpuloPlanetIcon", 64);
 SerpuloPlanetIcon.alwaysUnlocked = true;
 
+const KoluroStarIcon = createPhantomBlock("KoluroStarIcon", 64);
+
+const CarputoPlanetIcon = createPhantomBlock("CarputoPlanetIcon", 64);
+
+Events.run(Trigger.acceleratorUse, run(() => {
+KoluroStar.unlocked = true;
+CarputoPlanetIcon.unlocked = true;
+	
+testSector.unlocked = true;
+
+CarputoPlanet.accessible = true;
+}));
+
+
 TechNode(Blocks.coreShard, SunStarIcon, ItemStack.empty);//временно
 TechNode(SunStarIcon, SerpuloPlanetIcon, ItemStack.empty);
-//TechTree.TechNode(SerpuloPlanetIcon, TechTree.get(Blocks.coreShard), ItemStack.empty);
+TechNode(SunStarIcon, KoluroStarIcon, ItemStack.empty);
+TechNode(KoluroStarIcon, CarputoPlanetIcon, ItemStack.empty);
+
+
+//VANILLA TECH TREE//
 
 changeParent(SectorPresets.groundZero, SerpuloPlanetIcon);
 changeParent(SectorPresets.frozenForest, SectorPresets.groundZero);
